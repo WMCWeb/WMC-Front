fcc52862bc0015648c7268c0cd3a2e46c128d8a1
Daeho311 <67960726+Daeho311@users.noreply.github.com>
2020-08-07 14:53:57 +0900

    Update Account.js

--- src/components/page/Account.js
+++ src/components/page/Account.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Account2 from './Account_components/Account2';
+import Account2 from '.Account2';
 import Table from '@material-ui/core/Table';
 import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
